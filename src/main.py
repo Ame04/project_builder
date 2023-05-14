a4b6167119352f46afa1b9055201151bbf7cbf17
arme04 <rugby.man0406@gmail.com>
2023-05-14 18:52:02 +0200

    Added a project class to ahndle parser info more easily

--- src/main.py
+++ src/main.py
@@ -11,20 +11,42 @@ def get_parser() -> argparse.ArgumentParser:
                                     )
 
     # Add arguments
-    parser.add_argument("project_part",
+    parser.add_argument("repo_url",
+                        nargs="?",
+                        help="The URL of you repository to clone from"
+                        )
+    parser.add_argument("--interactive", "-i",
+                        action="store_true",
+                        help="To launch the scrypt in interactive mode"
+                        )
+    parser.add_argument("--output", "-o",
+                        default="./",
+                        help="The path where to put you new project"
+                        )
+    parser.add_argument("--project_part", "-p",
                         nargs="*",
                         choices=["meca", "elec", "code"],
                         help="The part you want to put in your project"
                         )
-    parser.add_argument("--interactive", "-i",
-                        action="store_true",
-                        help="To launch the scrypt in interactive mode")
-
     return parser
 
 def main(args):
     ''' The main part of the script '''
 
+class Project():
+    ''' Class to propagate informations between scripts '''
+    interactive:bool=False
+    output:str=None
+    project_part:list=None
+    repo_url:str=None
+
+    def __init__(self, args):
+        ''' Initialyse the class with info from the arg parser '''
+        self.interactive = args.interactive
+        self.output = args.output
+        if args.project_part is not None:
+            self.project_part = args.project_part
+        self.repo_url = args.repo_url
 
 if __name__=="__main__":
     # Parse the arguments
@@ -34,3 +56,4 @@ if __name__=="__main__":
     if args.interactive:
         print("Script launched in interactive mode")
 
+    project_opt = Project(args=args)
