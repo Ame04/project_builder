21e41758b0b0d6e8933546173b9075b006e3c399
arme04 <rugby.man0406@gmail.com>
2023-05-15 22:50:20 +0200

    Securised the cloning

--- src/main_folder_creation.py
+++ src/main_folder_creation.py
@@ -2,11 +2,12 @@
 
 import os
 import subprocess
+import utils
 
 def create_main_folder(project_opt):
     ''' Create the main folder and fill it '''
     if project_opt.interactive:
-        project_opt.output = input("Enter the path where you want to creat your project : ")
+        project_opt.output = input("Enter the path where you want to create your project : ")
 
     # Verify the path and create it if it does not exist
     if not os.path.exists(project_opt.output):
@@ -19,15 +20,16 @@ def create_main_folder(project_opt):
 
     cmd_line = "git clone " + project_opt.repo_url
 
-    print("Cloning the blank repository ...")
-    # Check if the path exists
+    print("Entering the folder and cloning the blank repository ...")
     os.chdir(project_opt.output)
-    print("The current working directory is there : " + os.getcwd())
     # Clone the repository
-    subprocess.run(cmd_line, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        subprocess.run(cmd_line, shell=True, check=True,
+                       stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError as e:
+        print(utils.color_red + "Cloning exited with return code : " +
+              str(e.returncode) + utils.color_reset)
+        print(e.stderr)
+        exit(1)
+
     print("Cloning the blank repository : done")
-
-
-
-
-
