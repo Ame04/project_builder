812146106cbb2304dbe2e3509f3286e55c9cf6ea
arme04 <rugby.man0406@gmail.com>
2023-05-15 22:42:21 +0200

    added utils files

---
+++ src/utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# ANSI escape codes for text colors
+color_reset = "\033[0m"
+color_red = "\033[31m"
+color_green = "\033[32m"
+color_yellow = "\033[33m"
+color_blue = "\033[34m"
+color_magenta = "\033[35m"
+color_cyan = "\033[36m"
